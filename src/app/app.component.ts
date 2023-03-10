948cd6f03d8ee5ee7ef0f459c1f3130404ee4731
Kholofelo Paka <kholofelo.paka@standardbank.co.za>
2023-03-10 03:47:08 +0200

    Test

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from "@angular/core";
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  title = "CodeSandbox";
+  private host: string= 'http://pakapokemon-env.eba-hshdfk2s.eu-north-1.elasticbeanstalk.com';
+  
+  constructor(private http: HttpClient) {
+    console.log("FD"); 
+    this.http.get<any>(this.host+"/pokemon?offset=100&limit=100").subscribe((data: any) => {
+     console.log(data); 
+    })
+  }
+}
